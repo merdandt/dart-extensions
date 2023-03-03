2892646b1821792633bebb6aeed26274e69a9b61
XSkills <meinnps@gmail.com>
2023-03-03 17:13:32 +0500

    03.03.23

--- extensions/iterable.dart
+++ extensions/iterable.dart
@@ -1,3 +1,4 @@
+import 'dart:math' as math show Random;
 // extension SumNumIterable<T extends num> on Iterable<T> {
 //   T sum() {
 //     // initialize sum
@@ -15,3 +16,23 @@
 extension OptionalFirst<T> on Iterable<T> {
   T? get optionalFirst => isEmpty ? null : first;
 }
+
+/// Example
+
+/// To get an random element of Iterable
+extension RandomElement<T> on Iterable<T> {
+  T get getRandom => elementAt(math.Random().nextInt(length));
+}
+
+/// Example
+/// var list = ['one', 'two', 'three', 'four'];
+/// final random = list.getRandom;
+
+/// Subscript to rely on getting proper data
+extension Subscript<T> on Iterable<T> {
+  T? operator [](int index) => length > index ? elementAt(index) : null;
+}
+/// Example:
+/// var list = ['foo', 'bar'];
+/// var item = list[10];
+
